809f2da9134c698527e60d7e6d129d85c5ea1870
DorEmAy <105840227+DorEmAy@users.noreply.github.com>
2022-11-08 10:16:34 +0900

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("main.js!!");
+
+// Ready
+$(document).ready(() => {
+    console.log("Ready!!");
+
+    const option = { responseType: "blob" };
+    axios
+        .get("./data.json", option)
+        .then((res) => {
+            // 通信が成功した場合
+            console.log("通信成功");
+            console.log(res);
+
+            res.data.text().then((str) => {
+                let arr = JSON.parse(str);
+                console.log(arr);
+            });
+        })
+        .catch((err) => {
+            // 通信が失敗した場合
+            console.log("通信失敗");
+            console.log(err);
+        });
+});
