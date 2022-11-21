1feac77c93b9e8226ea04426a3e317479d63b39b
DorEmAy <105840227+DorEmAy@users.noreply.github.com>
2022-11-22 08:31:42 +0900

    weatherapp

--- main.js
+++ main.js
@@ -14,55 +14,51 @@ $(document).ready(() => {
             res.data.text().then((str) => {
                 let arr = JSON.parse(str);
 
-                let today = new Date(); //今日
-                let tomorrow = new Date(); //明日
-                tomorrow.setDate(tomorrow.getDate() + 1);
-                let now_day = today.getDay(); //今日の曜日
-                let tomorrow_day = tomorrow.getDay(); //明日の曜日
-
-                //地名
-                let area_name = arr[0]["timeSeries"][0]["areas"][0]["area"]["name"];
-                $(".area_name").text(area_name);
-
-                //天気分
-                let w_name = arr[0]["timeSeries"][0]["areas"][0]["weathers"][0];
-                $(".w_text").text(w_name);
-
-                //天気アイコン
-                $(".main_2 img").attr("src", "./images/" + arr[0]["timeSeries"][0]["areas"][0]["weatherCodes"][0] + ".svg");
-
-                //気温
-                let temp_min = arr[0]["timeSeries"][2]["areas"][0]["temps"][0];
-                let temp_max = arr[0]["timeSeries"][2]["areas"][0]["temps"][1];
-                let temp_youbi = youbi(arr[0]["timeSeries"][2]["timeDefines"][1]);
-                if (temp_youbi != now_day) {
-                    //1つめのズレ
-                    temp_max = temp_min;
-                    temp_min = "";
-                }
-                temp_youbi = youbi(arr[0]["timeSeries"][2]["timeDefines"][0]);
-                if (temp_youbi != now_day) {
-                    //2つめのズレ
-                    temp_max = "";
-                }
-                $(".main_3 .temp_min").text("朝の最低気温  " + temp_min + (temp_min == "" ? "-" : "°"));
-                $(".main_3 .temp_max").text("昼の最高気温  " + temp_max + (temp_min == "" ? "-" : "°"));
-
-                //予報
-                let i = 0,
-                    j,
-                    c = 0,
-                    tr,
-                    info,
-                    t,
-                    at,
-                    idx;
-                const y = ["日", "月", "火", "水", "木", "金", "土"];
-
                 //対応表読み込み
                 axios.get("./trans.JSON", option).then((ress) => {
                     ress.data.text().then((strr) => {
-                        tr = JSON.parse(strr);
+                        let tr = JSON.parse(strr);
+
+                        let today = new Date(); //今日
+                        let tomorrow = new Date(); //明日
+                        tomorrow.setDate(tomorrow.getDate() + 1);
+                        let now_day = today.getDay(); //今日の曜日
+                        let tomorrow_day = tomorrow.getDay(); //明日の曜日
+
+                        //地名
+                        let area_name = arr[0]["timeSeries"][0]["areas"][0]["area"]["name"];
+                        $(".area_name").text(area_name);
+
+                        //天気アイコン
+                        $(".main_2 img").attr("src", "./images/" + tr[arr[0]["timeSeries"][0]["areas"][0]["weatherCodes"][0]]);
+
+                        //気温
+                        let temp_min = arr[0]["timeSeries"][2]["areas"][0]["temps"][0];
+                        let temp_max = arr[0]["timeSeries"][2]["areas"][0]["temps"][1];
+                        let temp_youbi = youbi(arr[0]["timeSeries"][2]["timeDefines"][1]);
+                        if (temp_youbi != now_day) {
+                            //1つめのズレ
+                            temp_max = temp_min;
+                            temp_min = "";
+                        }
+                        temp_youbi = youbi(arr[0]["timeSeries"][2]["timeDefines"][0]);
+                        if (temp_youbi != now_day) {
+                            //2つめのズレ
+                            temp_max = "";
+                        }
+                        $(".main_3 .temp_min").text("朝の最低気温  " + temp_min + (temp_min == "" ? "-" : "°"));
+                        $(".main_3 .temp_max").text("昼の最高気温  " + temp_max + (temp_min == "" ? "-" : "°"));
+
+                        //予報
+                        let i = 0,
+                            j,
+                            c = 0,
+                            info,
+                            t,
+                            at,
+                            idx;
+                        const y = ["日", "月", "火", "水", "木", "金", "土"];
+
                         let afteryoubi = youbi(arr[1]["timeSeries"][0]["timeDefines"][0]);
                         if (afteryoubi == now_day) {
                             i += 1;
